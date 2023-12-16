f5e9d802d4fa56d211b5855dcfe02398faba94ba
Sadiq Ahmed <offsznahmed@Sadiqs-MacBook-Pro.local>
2023-12-15 19:57:08 -0800

    Done

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import session from './session'
+import outfitsReducer from './outfit';
 
 const rootReducer = combineReducers({
   session,
+  outfits: outfitsReducer,
 });
 
 
